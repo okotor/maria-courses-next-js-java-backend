7c319b578976c0334ab1744f7a5c40f9bf20ea3f
Tom <cornelius.lundi@gmail.com>
2025-04-07 22:08:49 +0200

    deleted backend-java - now running separately

--- backend-java/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Load environment variables from .env file if it exists
-if [ -f /.env ]; then
-  export $(grep -v '^#' /.env | xargs -d '\n')
-fi
-
-# Run the Java application
-exec java -jar /backend-java.jar
