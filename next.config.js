e01d2792dbcc3332d149b5dbcbb17b9803c2973f
Tom <cornelius.lundi@gmail.com>
2025-04-07 22:24:24 +0200

    updated endpoints to fit deployed backend

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
-/** @type {import('next').NextConfig} */
-import dotenv from 'dotenv';
+// /** @type {import('next').NextConfig} */
+// import dotenv from 'dotenv';
 
 dotenv.config();
 
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'marian-courses-bucket.s3.us-east-1.amazonaws.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL, // Example: Making env variables available
-  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'marian-courses-bucket.s3.us-east-1.amazonaws.com',
+  //       port: '',
+  //       pathname: '/**',
+  //     },
+  //   ],
+  // },
+  // env: {
+  //   DATABASE_URL: process.env.DATABASE_URL, // Example: Making env variables available
+  // },
 };
 
 export default nextConfig;
