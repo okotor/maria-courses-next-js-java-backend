8193ef59700d939fd0a6ac20476dc4d5437bf775
Tom <cornelius.lundi@gmail.com>
2025-05-08 16:33:10 +0200

    Fixed All Courses Show

--- components/Courses/CoursesOverview.js
+++ components/Courses/CoursesOverview.js
@@ -1,50 +1,13 @@
 'use client';
 
-import React from 'react';
 import CourseSearchBar from './CourseSearchBar';
 import CoursesGrid from './CoursesGrid';
 
-
-class ErrorBoundary extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { hasError: false, error: null, errorInfo: null };
-    }
-  
-    static getDerivedStateFromError(error) {
-      return { hasError: true };
-    }
-  
-    componentDidCatch(error, errorInfo) {
-      console.error("Error caught in ErrorBoundary:", error, errorInfo);
-      this.setState({ error, errorInfo });
-    }
-  
-    render() {
-      if (this.state.hasError) {
-        return (
-          <div>
-            <h1>Something went wrong.</h1>
-            <details style={{ whiteSpace: 'pre-wrap' }}>
-              {this.state.error && this.state.error.toString()}
-              <br />
-              {this.state.errorInfo && this.state.errorInfo.componentStack}
-            </details>
-          </div>
-        );
-      }
-  
-      return this.props.children;
-    }
-  }
-
-const CoursesOverview = ({ courses, onSearch }) => {
+export default function CoursesOverview({ courses, onSearch }) {
   return (
-    <ErrorBoundary>
-      <CourseSearchBar onSearch={onSearch}/>
+    <>
+      <CourseSearchBar onSearch={onSearch} />
       <CoursesGrid courses={courses} />
-    </ErrorBoundary>
+    </>
   );
-};
-
-export default CoursesOverview;
+}
