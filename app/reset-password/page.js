600f4c4c4a235d24a30af79bd109d486be0753c3
Tom <cornelius.lundi@gmail.com>
2025-05-21 22:42:42 +0200

    reset password logic running & resneding reg confirmation

--- app/reset-password/page.js
+++ app/reset-password/page.js
@@ -1,11 +1,12 @@
 'use client';
 
+import { Suspense } from 'react';
 import ResetPasswordForm from '@/components/AuthForms/ResetPasswordForm';
 
 export default function ResetPasswordPage() {
   return (
-    <>
+    <Suspense fallback={<div>Načítání formuláře...</div>}>
       <ResetPasswordForm />
-    </>
+    </Suspense>
   );
 }
